3fd83fc4a8ad4ee98bf6ea6ecc4c70376ee25594
berry22jelly <153977953+berry22jelly@users.noreply.github.com>
2025-02-28 07:45:38 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 @echo off  
 SETLOCAL  
   
-:: 设置Python解释器的路径  
+:: 设置Python解释器的路径  TODO change to real path 
 set PYTHON_EXE=D:\py2.7\python2.exe  
 ::set PYTHON_EXE=python
 :: 使用Python解释器调用compileall模块来编译当前目录及其子目录下的所有.py文件  
